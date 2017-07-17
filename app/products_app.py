cb12c95510e87b3e84dc1373f0c6c11211b3d0be
Dan Mikus <daniel.j.mikus@gmail.com>
2017-07-17 19:47:23 -0400

    Created Header

--- app/products_app.py
+++ app/products_app.py
@@ -1,1 +1,22 @@
-print("Hello World")
+import getpass
+
+username = getpass.getuser()
+
+header = '''
+                        ---------------------
+                         PRODUCT APPLICATION
+                        ---------------------
+                          WELCOME {0}!
+
+    There are 21 products in the database. Please select an operation:
+
+        Operation | Description
+        --------- | ----------
+        'List'    | Display a list of product identifiers and names
+        'Show'    | Show information about a product
+        'Create'  | Add a new product
+        'Update'  | Edit an existing product
+        'Destroy' | Delete and existing product
+
+'''.format(username.upper())
+print(header)
