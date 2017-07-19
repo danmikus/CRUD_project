f03494ac611b129f2edb7b8db6eadb92fd97a545
Dan Mikus <daniel.j.mikus@gmail.com>
2017-07-19 18:19:13 -0400

    Refactored a bit more and added error handling to list and show.

--- app/products_app.py
+++ app/products_app.py
@@ -1,22 +1,45 @@
 import getpass
-import os
 import csv
 
 
-#    user_input = input("Ther: ")
-#    if user_input.strip() == "DONE":
-#        break
-#    try:
-#        int_input = int(user_input)
-#    except ValueError:
-#        print("ERROR: Please enter an integer")
-#        continue
-#    if int_input < 1 or int_input > len(products):
-#        print("ERROR: Please enter a product number between 1 and %s" % len(products))
-#        continue
-#    else:
-#        shopping_list.append(int(user_input))
-#return shopping_list
+
+
+
+
+def initialize(products):
+
+    username = getpass.getuser()
+    header = '''
+                            ---------------------
+                             PRODUCT APPLICATION
+                            ---------------------
+                              WELCOME {0}!
+
+        There are {1} products in the database. Please select an operation
+                   or type 'Quit' to exit the program:'''.format(username.upper(), len(products))
+
+    operations = '''
+            Operation | Description
+            --------- | ----------
+            'List'    | Display a list of product identifiers and names
+            'Show'    | Show information about a product
+            'Create'  | Add a new product
+            'Update'  | Edit an existing product
+            'Destroy' | Delete and existing product
+            'Quit'    | Exit the program
+            '''
+
+    print(header)
+    while True:
+        print(operations)
+        user_input = input("Please enter your selection:\n>").upper()
+        if user_input.strip() == "QUIT":
+            break
+        elif user_input == "LIST":
+            list_products(products)
+            print("\n")
+        elif user_input == "SHOW":
+            show_product(products)
 
 
 new_path = '../data/products.csv'
@@ -27,41 +50,31 @@ def list_products(products):
     for row in products:
         print(row["id"], "-", row["name"], "-", row["aisle"], "-", row["department"], "-", "$" + "{0:.2f}".format(float(row["price"])))
 
+def show_product(products):
 
-
-def show_product(input, products):
-
-    product = list(filter(lambda x: x["id"] == str(input), products))
-    print(product[0]["id"], "-", product[0]["name"], "-", product[0]["aisle"], "-", product[0]["department"], "-", "$" + "{0:.2f}".format(float(product[0]["price"])))
+    while True:
+        selection = input("Please enter a product ID or type 'Back':\n>")
+        if selection.upper() == "BACK":
+            break
+        try:
+            selection = int(selection)
+        except ValueError:
+            print("ERROR: Please enter an integer")
+            continue
+        if selection < 1 or selection > len(products):
+            print("ERROR: No product exists for this ID. Please enter a product number between 1 and {0}".format(len(products)))
+            continue
+        else:
+            product = list(filter(lambda x: x["id"] == str(selection), products))
+            print("\n" + product[0]["id"], "-", product[0]["name"], "-", product[0]["aisle"], "-", product[0]["department"], "-", "$" + "{0:.2f}".format(float(product[0]["price"])) + "\n")
 
 def main():
-    username = getpass.getuser()
     new_path = '../data/products.csv'
 
     with open(new_path, "r") as csv_file:
         reader = list(csv.DictReader(csv_file))
 
-    header = '''
-                            ---------------------
-                             PRODUCT APPLICATION
-                            ---------------------
-                              WELCOME {0}!
-
-        There are {1} products in the database. Please select an operation:
-
-            Operation | Description
-            --------- | ----------
-            'List'    | Display a list of product identifiers and names
-            'Show'    | Show information about a product
-            'Create'  | Add a new product
-            'Update'  | Edit an existing product
-            'Destroy' | Delete and existing product
-
-    '''.format(username.upper(), len(reader))
-
-    #list_products(reader)
-    show_product(2, reader)
-
+    initialize(reader)
 
 if __name__ == "__main__":
     main()
