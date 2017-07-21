5461a9d446e30380ab377982d04cdd60b2aa0373
Dan Mikus <daniel.j.mikus@gmail.com>
2017-07-21 02:56:27 -0400

    Started update function and refactored the read function

--- app/products_app.py
+++ app/products_app.py
@@ -1,13 +1,15 @@
 import getpass
 import csv
 
+def start_reader(path):
 
+    with open(path, "r") as csv_file:
+        reader = list(csv.DictReader(csv_file))
+    return reader
 
+def initialize(path):
 
-
-
-def initialize(products, path):
-
+    products = start_reader(path)
     username = getpass.getuser()
     header = '''
                             ---------------------
@@ -36,28 +38,31 @@ def initialize(products, path):
         if user_input.strip() == "QUIT":
             break
         elif user_input == "LIST":
-            list_products(products)
+            list_products(path)
             print("\n")
         elif user_input == "SHOW":
-            show_product(products)
+            show_product(path)
         elif user_input == "CREATE":
-            create_product(products, path)
+            create_product(path)
+        elif user_input == "UPDATE":
+            update_product(path)
         else:
             print("ERROR: Please enter a valid operation")
 
 
-new_path = '../data/products.csv'
+def list_products(path):
 
-def list_products(products):
+    products = start_reader(path)
 
     print("THERE ARE {0} PRODUCTS:\n".format(len(products)))
     for row in products:
         print(row["id"], "-", row["name"], "-", row["aisle"], "-", row["department"], "-", "$" + "{0:.2f}".format(float(row["price"])))
 
-def show_product(products):
+def show_product(path):
+
+    products = start_reader(path)
 
     while True:
-
         selection = input("Please enter a product ID or type 'Back':\n>")
         if selection.upper() == "BACK":
             break
@@ -73,21 +78,20 @@ def show_product(products):
             product = list(filter(lambda x: x["id"] == str(selection), products))
             print("\n" + product[0]["id"], "-", product[0]["name"], "-", product[0]["aisle"], "-", product[0]["department"], "-", "$" + "{0:.2f}".format(float(product[0]["price"])) + "\n")
 
-def create_product(products, path):
+def create_product(path):
 
     while True:
-        with open(new_path, "r") as csv_file:
-            prod_num = len(list(csv.DictReader(csv_file)))
+        prod_num = len(start_reader(path))
 
         new_product = {}
         print("Please enter the information for the new product:")
         new_product["id"] = prod_num + 1
-        new_product["name"] = input("Name:\n")
-        new_product["aisle"] = input("Aisle:\n")
-        new_product["department"] = input("Department:\n")
+        new_product["name"] = input("Name:\n>")
+        new_product["aisle"] = input("Aisle:\n>")
+        new_product["department"] = input("Department:\n>")
         while True:
             try:
-                new_product["price"] = float(input("price:\n"))
+                new_product["price"] = float(input("price:\n>"))
             except ValueError:
                 print("Error: Enter a valid price")
                 continue
@@ -98,7 +102,7 @@ def create_product(products, path):
             writer = csv.DictWriter(csv_file, fieldnames = ["id", "name", "aisle", "department", "price"])
             writer.writerow(new_product)
 
-        print("The product below has been successfully added: ")
+        print("\nThe product below has been successfully added: ")
         print(new_product["id"], "-", new_product["name"], "-", new_product["aisle"], "-", new_product["department"], "-", "$" + "{0:.2f}".format(float(new_product["price"])) + "\n")
 
         while True:
@@ -114,13 +118,30 @@ def create_product(products, path):
         else:
             break
 
+def update_product(path):
+
+    products = start_reader(path)
+
+    while True:
+        selection = input("Please enter the ID of the product you want to update or type 'Back':\n>")
+        if selection.upper() == "BACK":
+            break
+        try:
+            selection = int(selection)
+        except ValueError:
+            print("ERROR: Please enter an integer")
+            continue
+        if selection < 1 or selection > len(products):
+            print("ERROR: No product exists for this ID. Please enter a product number between 1 and {0}".format(len(products)))
+            continue
+        else:
+            product = list(filter(lambda x: x["id"] == str(selection), products))
+            
+
 def main():
-    new_path = '../data/products.csv'
 
-    with open(new_path, "r") as csv_file:
-        reader = list(csv.DictReader(csv_file))
-
-    initialize(reader, new_path)
+    prod_path = '../data/products.csv'
+    initialize(prod_path)
 
 if __name__ == "__main__":
     main()
