3a1be620d6a77a83aeb8591455db29d0c94efaef
Dan Mikus <daniel.j.mikus@gmail.com>
2017-07-23 04:35:41 -0400

    completed update and destroy. Main functionality now complete

--- app/products_app.py
+++ app/products_app.py
@@ -1,5 +1,6 @@
 import getpass
 import csv
+import collections
 
 def start_reader(path):
 
@@ -46,6 +47,8 @@ def initialize(path):
             create_product(path)
         elif user_input == "UPDATE":
             update_product(path)
+        elif user_input == "DESTROY":
+            destroy_product(path)
         else:
             print("ERROR: Please enter a valid operation")
 
@@ -54,7 +57,7 @@ def list_products(path):
 
     products = start_reader(path)
 
-    print("THERE ARE {0} PRODUCTS:\n".format(len(products)))
+    print("\nThere are {0} products:\n".format(len(products)))
     for row in products:
         print(row["id"], "-", row["name"], "-", row["aisle"], "-", row["department"], "-", "$" + "{0:.2f}".format(float(row["price"])))
 
@@ -63,7 +66,7 @@ def show_product(path):
     products = start_reader(path)
 
     while True:
-        selection = input("Please enter a product ID or type 'Back':\n>")
+        selection = input("\nPlease enter a product ID or type 'Back':\n>")
         if selection.upper() == "BACK":
             break
         try:
@@ -75,8 +78,10 @@ def show_product(path):
             print("ERROR: No product exists for this ID. Please enter a product number between 1 and {0}".format(len(products)))
             continue
         else:
-            product = list(filter(lambda x: x["id"] == str(selection), products))
-            print("\n" + product[0]["id"], "-", product[0]["name"], "-", product[0]["aisle"], "-", product[0]["department"], "-", "$" + "{0:.2f}".format(float(product[0]["price"])) + "\n")
+            product = list(filter(lambda x: x["id"] == str(selection), products))[0]
+            product["price"] = float(product["price"])
+            print("\n{id} - {name} - {aisle} - {department} - ${price:.2f}".format(**product))
+            #print("\n" + product[0]["id"], "-", product[0]["name"], "-", product[0]["aisle"], "-", product[0]["department"], "-", "$" + "{0:.2f}".format(float(product[0]["price"])))
 
 def create_product(path):
 
@@ -84,14 +89,14 @@ def create_product(path):
         prod_num = len(start_reader(path))
 
         new_product = {}
-        print("Please enter the information for the new product:")
+        print("\nPlease enter the information for the new product:")
         new_product["id"] = prod_num + 1
-        new_product["name"] = input("Name:\n>")
-        new_product["aisle"] = input("Aisle:\n>")
-        new_product["department"] = input("Department:\n>")
+        new_product["name"] = input("\nName:\n>")
+        new_product["aisle"] = input("\nAisle:\n>")
+        new_product["department"] = input("\nDepartment:\n>")
         while True:
             try:
-                new_product["price"] = float(input("price:\n>"))
+                new_product["price"] = float(input("\nprice:\n>"))
             except ValueError:
                 print("Error: Enter a valid price")
                 continue
@@ -103,16 +108,15 @@ def create_product(path):
             writer.writerow(new_product)
 
         print("\nThe product below has been successfully added: ")
-        print(new_product["id"], "-", new_product["name"], "-", new_product["aisle"], "-", new_product["department"], "-", "$" + "{0:.2f}".format(float(new_product["price"])) + "\n")
+        print("{id} - {name} - {aisle} - {department} - ${price:.2f}\n".format(**new_product))
 
         while True:
-            selection = input("Do you want to input another product? y/n:\n").upper()
+            selection = input("Do you want to input another product? y/n:\n>").upper()
             if selection != "Y" and selection != "N":
                 print("ERROR: Invalid input")
                 continue
             else:
                 break
-
         if selection == "Y":
             continue
         else:
@@ -120,10 +124,10 @@ def create_product(path):
 
 def update_product(path):
 
-    products = start_reader(path)
-
     while True:
-        selection = input("Please enter the ID of the product you want to update or type 'Back':\n>")
+        products = start_reader(path)
+        updated_product = collections.OrderedDict()
+        selection = input("\nPlease enter the ID of the product you want to update, or type 'Back':\n>")
         if selection.upper() == "BACK":
             break
         try:
@@ -135,8 +139,78 @@ def update_product(path):
             print("ERROR: No product exists for this ID. Please enter a product number between 1 and {0}".format(len(products)))
             continue
         else:
-            product = list(filter(lambda x: x["id"] == str(selection), products))
-            
+            product = (list(filter(lambda x: x["id"] == str(selection), products)))[0]
+            ind = products.index(product)
+
+            updated_product["id"] = product["id"]
+            updated_product["name"] = input("Input the product name to replace '{0}':\n>".format(product["name"]))
+            updated_product["aisle"] = input("Input the aisle to replace '{0}':\n>".format(product["aisle"]))
+            updated_product["department"] = input("Input the department to replace '{0}':\n>".format(product["department"]))
+            while True:
+                try:
+                    updated_product["price"] = float(input("Input the price to replace '{0}':\n>".format(product["price"])))
+                except ValueError:
+                    print("Error: Enter a valid price")
+                    continue
+                else:
+                    break
+
+            products.pop(ind)
+            products.insert(ind, updated_product)
+
+            with open(path, "w") as csv_file:
+                writer = csv.DictWriter(csv_file, fieldnames = ["id", "name", "aisle", "department", "price"])
+                writer.writeheader()
+                writer.writerows(products)
+
+            print("\nThe product below has been successfully updated:")
+            print("{id} - {name} - {aisle} - {department} - ${price:.2f}".format(**updated_product)) #TODO change to truncate
+
+
+def destroy_product(path):
+
+    while True:
+        products = start_reader(path)
+        selection = input("\nPlease enter the ID of the product you want to delete, or type 'Back':\n>")
+        if selection.upper() == "BACK":
+            break
+        try:
+            selection = int(selection)
+        except ValueError:
+            print("ERROR: Please enter an integer")
+            continue
+        if selection < 1 or selection > len(products):
+            print("ERROR: No product exists for this ID. Please enter a product number between 1 and {0}".format(len(products)))
+            continue
+        else:
+            product = (list(filter(lambda x: x["id"] == str(selection), products)))[0]
+            product["price"] = float(product["price"])
+
+            while True:
+                confirmation = input("Are you sure you want to delete {0}? y/n:\n>".format(product["name"])).upper()
+                if confirmation != "Y" and confirmation != "N":
+                    print("ERROR: Invalid input")
+                    continue
+                else:
+                    break
+            if confirmation == "Y":
+                ind = products.index(product)
+                products.pop(ind)
+                new_id = range(1, (len(products) + 1))
+
+                for index, prod in enumerate(products):
+                    prod["id"] = new_id[index]
+
+                with open(path, "w") as csv_file:
+                    writer = csv.DictWriter(csv_file, fieldnames = ["id", "name", "aisle", "department", "price"])
+                    writer.writeheader()
+                    writer.writerows(products)
+
+                print("\nThe product below has been successfully deleted:")
+                print("{id} - {name} - {aisle} - {department} - ${price:.2f}".format(**product))
+            else:
+                continue
+
 
 def main():
 
